3240e896e834f34036a14a4abd56691ef14ddc4d
Ramkumar S <ajithram1112@gmail.comm>
2026-08-27 11:19:14 +0000

    Update bastion SSH access IP

--- security_groups.tf
+++ security_groups.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["49.37.223.5/32"]
+    cidr_blocks = ["49.43.249.173/32"]
   }
 
   egress {
